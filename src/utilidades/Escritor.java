cbe5de8c2b631f737c552ce1c5e2e5830e403583
Andoni84 <45061143+Andoni84@users.noreply.github.com>
2018-11-15 11:02:39 +0100

    Crear Escritor. Modificar Lector.

---
+++ src/utilidades/Escritor.java
@@ -0,0 +1,45 @@
+package utilidades;
+
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Escritor {
+
+	
+	/**
+	 * Clase Escritor
+	 * 
+	 * Contiene  un metodo escritura.
+	 * 
+	 * 15/11/2018
+	 *
+	 * @author Andoni
+	 * @version v1
+	 */
+	
+	
+	
+	
+	
+	//Utilidad escritura.Imprime en consola.
+	
+	private static Logger logger;
+
+	static {
+		try {
+			logger = LogManager.getLogger(Lector.class);
+		} catch (Throwable e) {
+			System.out.println("Logger Don't work");
+		}
+	}
+	
+	
+	public static void write(String msg) throws Exception {
+		System.out.println(msg);
+		logger.debug("Escritura correcta");
+	
+    }
+	
+}
--- src/utilidades/Lector.java
+++ src/utilidades/Lector.java
@@ -2,6 +2,9 @@ package utilidades;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Lector {
 	
 	/**
@@ -19,37 +22,46 @@ public class Lector {
 	
 	
 	
-	
 	//Utilidade de lectura, leen una entrada dependiendo del tipo de variables que se quiera obtener. 
 	//Algunas muestran por pantalla un mensaje antes de la recogida de datos.
 	//Propaga las excepciones.
 	
+	private static Logger logger;
+
+	static {
+		try {
+			logger = LogManager.getLogger(Lector.class);
+		} catch (Throwable e) {
+			System.out.println("Logger Don't work");
+		}
+	}
 	
-	public static String leerString() throws Exception {
+	
+	public static String readString() throws Exception {
 		String dato = new Scanner(System.in).nextLine();
 		logger.debug("Valor de lectura: "+dato);
 		return dato;
     }
 	
-	public static int leerInt() throws Exception {
+	public static int readInt() throws Exception {
 		int dato = new Scanner(System.in).nextInt();
 		logger.debug("Valor de lectura	: "+dato);
         return dato;
     }
 	
-	 public static int leerInt(String msg) throws Exception {
-	        System.out.println(msg);
-	        return leerInt();
+	 public static int readInt(String msg) throws Exception {
+	        Escritor.write(msg);
+	        return readInt();
 	}      
 	 
-	 public static double leerDouble() throws Exception {
+	 public static double readDouble() throws Exception {
 	        double dato = new Scanner(System.in).nextDouble();
 	        return dato;
 	    }
 	 
-	 public static double leerDouble(String msg) throws Exception {
-	        System.out.println(msg);
-	        return leerDouble();
+	 public static double readDouble(String msg) throws Exception {
+	        Escritor.write(msg);
+	        return readDouble();
 	    }
 	 
 }
